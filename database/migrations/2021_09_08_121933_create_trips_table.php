488292f2368f7af6989f15a3ebac8438b7a76687
Luca Giozza <luca.giozza@gmail.com>
2021-09-08 14:49:31 +0200

    first commit

---
+++ database/migrations/2021_09_08_121933_create_trips_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('agenzia', 100);
+            $table->string('durata_viaggio', 50);
+            $table->float('prezzo', 7, 2);
+            $table->string('destinazione',100);
+            $table->string('disponibilità',);
+            $table->float('numero_viaggio', 9, 2 );
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * 
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
